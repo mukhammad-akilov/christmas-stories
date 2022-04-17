06faf6c920bf63adf77f76d5cec5162637af8517
Mukhammad Akilov <muhammad_akilov@mail.ru>
2022-04-17 16:09:22 +0500

    Add: test CountDownTimer component. Bug fixing: fixed several bugs

--- src/components/ConfrimAudioModal/ConfirmAudioModal.jsx
+++ src/components/ConfrimAudioModal/ConfirmAudioModal.jsx
@@ -4,7 +4,6 @@ import PropTypes from 'prop-types';
 
 const ConfirmAudioModal = ({open, onClose, ...props}) => {
     // console.log("audio modal render");
-
     return (
         <>
             {open &&
